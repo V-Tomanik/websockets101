bfa8ff8ea6d7f4ca45dfb021a1f91338b7705247
vv <victor.o.tomanik@gmail.com>
2021-09-19 21:09:24 -0300

    Melhorando a implementação em python

--- python/client.py
+++ python/client.py
@@ -1,14 +1,16 @@
 import asyncio
-import websockets
+from websockets import client
 
 async def listen():
+    """
+    Função de cliente, assim que conectar com o servidor, envia uma mensagem e espera 
+    retornos"""
     url = "ws://127.0.0.1:8765"
-    async with websockets.connect(url) as websocket:
-        await websocket.send("Conectando")
+    async with client.connect(url) as c:
+        await c.send("Conectando")
         while True:
             #Espera uma resposta
-            msg = await websocket.recv()
+            msg = await c.recv()
             print(msg)
-
 
 asyncio.get_event_loop().run_until_complete(listen())
--- python/server.py
+++ python/server.py
@@ -1,20 +1,30 @@
 import asyncio
-import websockets
+from websockets import exceptions
+from websockets import server 
 
-
+#Porta que eu vou usar
 port = 8765
 print(f"Iniciando Server no port {port}")
 
+#Todas as rotinas que o server performar precisa receber tanto a conecção "websocket" quando o path
 async def echo(websocket,path):
+    """
+    Função de criação do servidor, espera a entrada de sockets e reenvia a mensagem que foi recebida para o mesmo socket,
+    os outros clientes não recebem
+    """
     print("Nova conexão no server")
     try:
         async for message in websocket:
             print(f"Mensagem recebida: {message}")
             await websocket.send(message)
-    except websockets.exceptions.ConnectionClosed:
+    except exceptions.ConnectionClosed:
         print("Cliente desconectado")
 
 async def echo_brodcast(websocket,path):
+    """
+    Função de criação de um servidor, lista todas as conecções, quando recebe uma mensagem envia para todos os outros clientes
+    exeto para original
+    """
     print("Nova conexão no server")
     clients_connected.add(websocket)
     try:
@@ -24,7 +34,8 @@ async def echo_brodcast(websocket,path):
                 if conn != websocket:
                     print(f"Mensagem recebida: {message}")
                     await conn.send(f"Outro cliente {message}")
-    except websockets.exceptions.ConnectionClosed:
+    #Caso um socket se desconecte, para não dar erro
+    except exceptions.ConnectionClosed:
         print("Cliente desconectado")
     finally:
         #Retira da lista o websocket que deslogou
@@ -32,7 +43,8 @@ async def echo_brodcast(websocket,path):
 
 
 async def main():
-    async with websockets.serve(echo_brodcast, "localhost", port):
+    #Função de criação do servidor usando uma co-rotina no path localhost na porta x
+    async with server.serve(echo, "localhost", port):
         await asyncio.Future()
 
 
